59d1b348007ac7f05f57ed5739ff4f8b586f4d50
Naman khandelwal <namankoolwal1411@gmail.com>
2022-06-16 20:31:11 +0530

    Initial commit

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -12,19 +12,19 @@ const Footer = () => {
                <i className="fa fa-copyright me-5"> 2022 NAMAN KHANDELWAL</i>
              </h5>         
              <div className="nav-item " id="navbarSupportedContent">
-               <a href="https:instagram.com/naman_koolwal"> 
+               <a href="https://instagram.com/naman_koolwal"> 
                  <i className="fa fa-instagram me-4"> Instagram</i>
                </a>
-               <a href="https:www.facebook.com/naman.koolwal"> 
+               <a href="https://www.facebook.com/naman.koolwal"> 
                  <i className="fa fa-facebook me-4"> Facebook</i>
                </a>
-               <a href="https:twitter.com/NAMANKH31936926?t=NgPyoRrpwAqAYonRRIUXxA&s=08"> 
+               <a href="https://twitter.com/NAMANKH31936926?t=NgPyoRrpwAqAYonRRIUXxA&s=08"> 
                  <i className="fa fa-twitter me-4"> Twitter</i>
                </a>
-               <a href="https:github.com/namankoolwal">
+               <a href="https://github.com/namankoolwal">
                  <i className="fa fa-github me-4"> Github</i>
                </a>
-               <a href="https:www.linkedin.com/in/naman-khandelwal-568971189/">
+               <a href="https://www.linkedin.com/in/naman-khandelwal-568971189/">
                  <i className="fa fa-linkedin me-4"> Linkedin</i>
                </a>
                <a href="mailto:namankoolwal1411@gmail.com">
