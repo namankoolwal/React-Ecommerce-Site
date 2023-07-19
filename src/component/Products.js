69875c4592a960e5fadc1e09194dd5fa4445ebed
Naman khandelwal <104143943+namankoolwal@users.noreply.github.com>
2023-07-19 12:49:20 +0530

    Update Products.js

--- src/component/Products.js
+++ src/component/Products.js
@@ -69,7 +69,7 @@ const Products = () => {
                   <div className="card-body">
                     <h5 className="card-title mb-0">{product.title.substring(0,12)}...</h5>
                     <p className="card-text lead fw-bold">
-                     ${product.price}
+                      &#x20a8; {(product.price*100)/2} 
                     </p>
                     <NavLink to={`/products/${product.id}`} className="btn btn-outline-dark">
                       Buy Now
